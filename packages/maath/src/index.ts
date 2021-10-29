32303b12009e3cac33c5c86ca134b079b59e7abf
Gianmarco Simone <gsimone@hey.com>
2021-10-29 23:22:04 +0200

    some geometry

--- packages/maath/src/index.ts
+++ packages/maath/src/index.ts
@@ -1,4 +1,4 @@
-import { MathUtils } from "three";
+import { MathUtils, Vector2, Vector3 } from "three";
 
 import type { TypedArray } from "./types";
 
@@ -58,4 +58,51 @@ export function vectorEquals(a, b, eps = Number.EPSILON) {
     Math.abs(a.y - b.y) < eps &&
     Math.abs(a.z - b.z) < eps
   );
+}
+
+
+type MyVector2 = number[] // fun fact, I think three's types aren't updated, vectors SHOULD be iterable in latest
+
+/**
+ * Uses triangle area determinant to check if 3 points are collinear.
+ * If they are, they can't make a triangle, so the determinant for the area will be 0!
+ * 
+ * Fun fact, you can use this same determinant to check the order of the points in the triangle
+ */
+export function collinear(points: MyVector2[]) {
+  /**
+    1/2 * | x1 - x2     x2 - x3 |
+          | y1 - y2     y2 - y3 |
+  */
+  const [[x1, y1], [x2, y2], [x3, y3]] = points
+
+  const a = x1 - x2
+  const b = x2 - x3
+  const c = y1 - y2
+  const d = y2 - y3
+
+  const det = a * d - b * c
+
+  const areaOfTriangle = det * 0.5
+
+  return areaOfTriangle === 0
+}
+
+// From https://algorithmtutor.com/Computational-Geometry/Determining-if-two-consecutive-segments-turn-left-or-right/
+const mv1 = new Vector2()
+const mv2 = new Vector2()
+
+export function doThreePointsMakeARight(points: MyVector2[]) {
+  const [p1, p2, p3] = points
+
+  if (collinear(points)) return false
+
+  // @ts-ignore 
+  const p2p1 = mv1.subVectors(p2, p1)
+  // @ts-ignore 
+  const p3p1 = mv2.subVectors(p3, p1)
+
+  const cross = p3p1.cross(p2p1)
+
+  if (cross > 0) return true
 }
