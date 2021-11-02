ca539bfa8bc4409f217101b180c0df29cf00bb0b
Gianmarco Simone <gsimone@hey.com>
2021-11-02 19:45:41 +0100

    box & friends

--- packages/maath/src/random.ts
+++ packages/maath/src/random.ts
@@ -2,13 +2,29 @@ import type { TypedArray } from "./types";
 
 const TAU = Math.PI * 2;
 
-// TODO Fix center offset
-// random on surface of sphere https://twitter.com/fermatslibrary/status/1430932503578226688
-export function onSphere(
-  buffer: TypedArray,
-  { radius = 1, center = [0, 0, 0] }
-) {
-  const random = () => Math.random();
+/***
+ * [3D] Sphere
+ */
+type Sphere = {
+  radius?: number;
+  center?: number[];
+};
+
+const defaultSphere = {
+  radius: 1,
+  center: [0, 0, 0],
+};
+
+const random = () => Math.random();
+
+// random on surface of sphere
+// - https://twitter.com/fermatslibrary/status/1430932503578226688
+// - https://mathworld.wolfram.com/SpherePointPicking.html
+export function onSphere(buffer: TypedArray, sphere?: Sphere) {
+  const { radius, center } = {
+    ...defaultSphere,
+    ...sphere,
+  };
 
   for (let i = 0; i < buffer.length; i += 3) {
     const u = random();
@@ -17,22 +33,20 @@ export function onSphere(
     const theta = Math.acos(2 * v - 1);
     const phi = TAU * u;
 
-    buffer[i] = Math.sin(theta) * Math.cos(phi) * radius;
-    buffer[i + 1] = Math.sin(theta) * Math.sin(phi) * radius;
-    buffer[i + 2] = Math.cos(theta) * radius;
+    buffer[i] = Math.sin(theta) * Math.cos(phi) * radius + center[0];
+    buffer[i + 1] = Math.sin(theta) * Math.sin(phi) * radius + center[1];
+    buffer[i + 2] = Math.cos(theta) * radius + center[2];
   }
 
-  return buffer
+  return buffer;
 }
 
-// TODO Fix center offset
 // from "Another Method" https://datagenetics.com/blog/january32020/index.html
-export function inSphere(
-  buffer: TypedArray,
-  { radius = 1, center = [0, 0, 0] }
-) {
-  const random = () => Math.random();
-
+export function inSphere(buffer: TypedArray, sphere?: Sphere) {
+  const { radius, center } = {
+    ...defaultSphere,
+    ...sphere,
+  };
   for (let i = 0; i < buffer.length; i += 3) {
     const u = Math.pow(random(), 1 / 3);
 
@@ -46,20 +60,33 @@ export function inSphere(
     y = (u * y) / mag;
     z = (u * z) / mag;
 
-    buffer[i] = x * radius;
-    buffer[i + 1] = y * radius;
-    buffer[i + 2] = z * radius;
+    buffer[i] = x * radius + center[0];
+    buffer[i + 1] = y * radius + center[1];
+    buffer[i + 2] = z * radius + center[2];
   }
 
-  return buffer
+  return buffer;
 }
 
+/***
+ * [2D] Circle
+ */
+type Circle = {
+  radius?: number;
+  center?: number[];
+};
+
+const defaultCircle = {
+  radius: 1,
+  center: [0, 0],
+};
+
 // random circle https://stackoverflow.com/a/50746409
-export function inCircle(
-  buffer: TypedArray,
-  { radius = 1, center = [0, 0] }
-) {
-  const random = () => Math.random();
+export function inCircle(buffer: TypedArray, circle?: Circle) {
+  const { radius, center } = {
+    ...defaultCircle,
+    ...circle,
+  };
 
   for (let i = 0; i < buffer.length; i += 2) {
     const r = radius * Math.sqrt(random());
@@ -69,14 +96,14 @@ export function inCircle(
     buffer[i + 1] = Math.cos(theta) * r + center[1];
   }
 
-  return buffer
+  return buffer;
 }
 
-export function onCircle(
-  buffer: TypedArray,
-  { radius = 1, center = [0, 0] }
-) {
-  const random = () => Math.random();
+export function onCircle(buffer: TypedArray, circle?: Circle) {
+  const { radius, center } = {
+    ...defaultCircle,
+    ...circle,
+  };
 
   for (let i = 0; i < buffer.length; i += 2) {
     const theta = random() * TAU;
@@ -85,27 +112,75 @@ export function onCircle(
     buffer[i + 1] = Math.cos(theta) * radius + center[1];
   }
 
-  return buffer
+  return buffer;
 }
 
-export function inBox(
-  buffer: TypedArray,
-  { sides = 1, center = [0, 0, 0] }: {
-    sides: number | number[],
-    center?: number[]
+/**
+ * [2D] Plane
+ */
+type Rect = {
+  sides: number | number[];
+};
+
+const defaultRect = {
+  sides: 1,
+};
+
+export function inRect(buffer: TypedArray, rect?: Rect) {
+  return buffer;
+}
+
+export function onRect(buffer: TypedArray, rect?: Rect) {
+  return buffer;
+}
+
+/***
+ * [3D] Box
+ */
+export function inBox(buffer: TypedArray, box?: Box) {
+  const { sides, center } = {
+    ...defaultBox,
+    ...box,
+  };
+
+  const sideX = typeof sides === "number" ? sides : sides[0];
+  const sideY = typeof sides === "number" ? sides : sides[1];
+  const sideZ = typeof sides === "number" ? sides : sides[2];
+
+  for (let i = 0; i < buffer.length; i += 3) {
+    buffer[i] = (Math.random() - 0.5) * sideX + center[0];
+    buffer[i + 1] = (Math.random() - 0.5) * sideY + center[1];
+    buffer[i + 2] = (Math.random() - 0.5) * sideZ + center[2];
   }
-) {
 
-  const sideX = typeof sides === "number" ? sides : sides[0]
-  const sideY = typeof sides === "number" ? sides : sides[1]
-  const sideZ = typeof sides === "number" ? sides : sides[2]
+  return buffer;
+}
 
-  
-  for (let i = 0; i < buffer.length; i+=3) {
-    buffer[i] = (Math.random() - .5) * sideX + center[0]
-    buffer[i+1] = (Math.random() - .5) * sideY + center[1]
-    buffer[i+2] = (Math.random() - .5) * sideZ + center[2]
+type Box = {
+  sides?: number[] | number;
+  center?: number[];
+};
+
+const defaultBox = {
+  sides: 1,
+  center: [0, 0, 0],
+};
+
+export function onBox(buffer: TypedArray, box?: Box) {
+  const { sides, center } = {
+    ...defaultBox,
+    ...box,
+  };
+
+  const sideX = typeof sides === "number" ? sides : sides[0];
+  const sideY = typeof sides === "number" ? sides : sides[1];
+  const sideZ = typeof sides === "number" ? sides : sides[2];
+
+  for (let i = 0; i < buffer.length; i += 3) {
+    buffer[i] = (Math.random() - 0.5) * sideX + center[0];
+    buffer[i + 1] = (Math.random() - 0.5) * sideY + center[1];
+    buffer[i + 2] = (Math.random() - 0.5) * sideZ + center[2];
   }
 
-  return buffer
+  return buffer;
 }
