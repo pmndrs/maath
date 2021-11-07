ffaaef07e3b58ac48f45ee19af1a8d2a92e82e19
Gianmarco Simone <gsimone@hey.com>
2021-11-07 15:41:22 +0100

    .

--- demo/src/sandboxes/circumcircle/src/index.tsx
+++ demo/src/sandboxes/circumcircle/src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <color attach="background" args={['#000']} />
+      <App />
+    </Canvas>
   </React.StrictMode>,
   rootElement
-)
+);
