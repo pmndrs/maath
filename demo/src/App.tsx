9bde6c0cb631299869e75cfad061a506e82545cc
Gianmarco Simone <gsimone@hey.com>
2021-11-07 15:16:56 +0100

    csb ci

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -13,6 +13,8 @@ import Points from "./Points";
 import { getCircumcircle } from "maath/triangle";
 import { lerp } from "maath/misc";
 
+import CircumcircleDemo from './sandboxes/circumcircle/src/App' 
+
 function ConvexHullDemo() {
   const pointsRef = useRef<THREE.Points>(null!);
   const lineRef = useRef<any>(null!);
@@ -55,107 +57,6 @@ function ConvexHullDemo() {
   );
 }
 
-function TrianglesDemo() {
-  const pointsRef = useRef<THREE.Points>(null!);
-  const lineRef = useRef<any>(null!);
-  const circleRef = useRef<any>(null!);
-  const circle2Ref = useRef<any>(null!);
-
-  const [{ points, pointsB, final }] = useState(() => {
-    // generate two sets of 4 points in a circle, that we'll use for our circumcircle visualization
-    const points = random.inCircle(new Float32Array(4 * 2), { radius: 0.4 });
-    const pointsB = random.inCircle(points.slice(0), { radius: 0.4 });
-    const final = pointsB.slice(0);
-
-    return { points, pointsB, final };
-  });
-
-  useFrame(({ clock }, dt) => {
-    const t = Math.sin(clock.getElapsedTime()) + 1 * 0.5;
-
-    // lerp between the two different sets of point, thus animating the two visualized triangles
-    buffer.lerp(points, pointsB, final, t);
-
-    // get the points as an array, to use them to create the geometries
-    const [a, b, c, d] = buffer.toVectorArray(final, 2);
-
-    // prettier-ignore
-    lineRef.current.geometry.setFromPoints([ 
-      a , b , c, a,
-      b, c, d, b
-    ], 2);
-
-    {
-      // get the circumcircle for the first triangle
-      const circle = getCircumcircle([
-        [a.x, a.y],
-        [b.x, b.y],
-        [c.x, c.y],
-      ]);
-
-      const curve = new EllipseCurve(
-        circle!.x,
-        circle!.y,
-        circle!.r,
-        circle!.r,
-        0,
-        2 * Math.PI,
-        false,
-        0
-      );
-
-      circleRef.current.geometry.setFromPoints(curve.getPoints(128));
-    }
-
-    {
-      // get the circumcircle for the second triangle
-      const circle = getCircumcircle([
-        [b.x, b.y],
-        [c.x, c.y],
-        [d.x, d.y],
-      ]);
-
-      const curve = new EllipseCurve(
-        circle!.x,
-        circle!.y,
-        circle!.r,
-        circle!.r,
-        0,
-        2 * Math.PI,
-        false,
-        0
-      );
-
-      circle2Ref.current.geometry.setFromPoints(curve.getPoints(128));
-    }
-
-    // update the points geometry since we imperatively updated the positions buffer
-    pointsRef.current.geometry.attributes.position.needsUpdate = true;
-  });
-
-  return (
-    <>
-      <Points points={final} stride={2} ref={pointsRef}>
-        <pointsMaterial size={5} />
-      </Points>
-
-      <line castShadow ref={lineRef}>
-        <bufferGeometry />
-        <lineBasicMaterial />
-      </line>
-
-      <line castShadow ref={circleRef}>
-        <bufferGeometry />
-        <lineBasicMaterial />
-      </line>
-
-      <line castShadow ref={circle2Ref}>
-        <bufferGeometry />
-        <lineBasicMaterial />
-      </line>
-    </>
-  );
-}
 
 const rotationAxis = new Vector3(1, 1, 0).normalize();
 const q = new Quaternion();
@@ -282,7 +183,7 @@ function Scene() {
 
       <Demo color="#ffaf80">
         <group scale={0.8} position-z={-0.3}>
-          <TrianglesDemo />
+          <CircumcircleDemo />
         </group>
       </Demo>
 
---
+++ demo/src/sandboxes/circumcircle/src/App.tsx
@@ -0,0 +1,114 @@
+import { useRef, useState } from "react";
+
+import { useFrame } from "@react-three/fiber";
+import { EllipseCurve } from "three";
+
+import * as random from "maath/random";
+import * as buffer from "maath/buffer";
+
+import Points from "../../../Points";
+import { getCircumcircle } from "maath/triangle";
+
+function TrianglesDemo() {
+  const pointsRef = useRef<THREE.Points>(null!);
+  const lineRef = useRef<any>(null!);
+  const circleRef = useRef<any>(null!);
+  const circle2Ref = useRef<any>(null!);
+
+  const [{ points, pointsB, final }] = useState(() => {
+    // generate two sets of 4 points in a circle, that we'll use for our circumcircle visualization
+    const points = random.inCircle(new Float32Array(4 * 2), { radius: 0.4 });
+    const pointsB = random.inCircle(points.slice(0), { radius: 0.4 });
+    const final = pointsB.slice(0);
+
+    return { points, pointsB, final };
+  });
+
+  useFrame(({ clock }, dt) => {
+    const t = Math.sin(clock.getElapsedTime()) + 1 * 0.5;
+
+    // lerp between the two different sets of point, thus animating the two visualized triangles
+    buffer.lerp(points, pointsB, final, t);
+
+    // get the points as an array, to use them to create the geometries
+    const [a, b, c, d] = buffer.toVectorArray(final, 2);
+
+    // prettier-ignore
+    lineRef.current.geometry.setFromPoints([ 
+      a , b , c, a,
+      b, c, d, b
+    ], 2);
+
+    {
+      // get the circumcircle for the first triangle
+      const circle = getCircumcircle([
+        [a.x, a.y],
+        [b.x, b.y],
+        [c.x, c.y],
+      ]);
+
+      const curve = new EllipseCurve(
+        circle!.x,
+        circle!.y,
+        circle!.r,
+        circle!.r,
+        0,
+        2 * Math.PI,
+        false,
+        0
+      );
+
+      circleRef.current.geometry.setFromPoints(curve.getPoints(128));
+    }
+
+    {
+      // get the circumcircle for the second triangle
+      const circle = getCircumcircle([
+        [b.x, b.y],
+        [c.x, c.y],
+        [d.x, d.y],
+      ]);
+
+      const curve = new EllipseCurve(
+        circle!.x,
+        circle!.y,
+        circle!.r,
+        circle!.r,
+        0,
+        2 * Math.PI,
+        false,
+        0
+      );
+
+      circle2Ref.current.geometry.setFromPoints(curve.getPoints(128));
+    }
+
+    // update the points geometry since we imperatively updated the positions buffer
+    pointsRef.current.geometry.attributes.position.needsUpdate = true;
+  });
+
+  return (
+    <>
+      <Points points={final} stride={2} ref={pointsRef}>
+        <pointsMaterial size={5} />
+      </Points>
+
+      <line castShadow ref={lineRef}>
+        <bufferGeometry />
+        <lineBasicMaterial />
+      </line>
+
+      <line castShadow ref={circleRef}>
+        <bufferGeometry />
+        <lineBasicMaterial />
+      </line>
+
+      <line castShadow ref={circle2Ref}>
+        <bufferGeometry />
+        <lineBasicMaterial />
+      </line>
+    </>
+  );
+}
+
+export default TrianglesDemo
