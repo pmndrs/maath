72580fcdcd9f950872b660655207dca05732103c
Gianmarco Simone <gsimone@hey.com>
2021-11-16 23:27:00 +0100

    feat: adds some helpers to v3

--- packages/maath/src/vector3.ts
+++ packages/maath/src/vector3.ts
@@ -28,31 +28,41 @@ export function subValue(a: V3, n: number): V3 {
 }
 
 export function scale(a: V3, n: number): V3 {
-  return [a[0] * n, a[1] * n, a[2] * n]
+  return [a[0] * n, a[1] * n, a[2] * n];
 }
 
-// export function dot(a: V3, b: V3): number {
-//   return a[0] * b[0] + a[1] * b[1];
-// }
+export function dot(a: V3, b: V3): number {
+  return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+}
+
+export function cross(a: V3, b: V3): V3 {
+  const x = a[1] * b[2] - a[2] * b[1];
+  const y = a[2] * b[0] - a[0] * b[2];
+  const z = a[0] * b[1] - a[1] * b[0];
+
+  return [x,y,z]
+}
 
 /**
  * Calculate the squared length of a vector.
  * Use this when comparing two vectors instead of length, as it's more efficient (no sqrt)
  */
-// export function lengthSqr(a: V3): number {
-//   return a[0] * a[0] + a[1] * a[1];
-// }
+export function lengthSqr(a: V3): number {
+  return a[0] * a[0] + a[1] * a[1] + a[2] * a[2];
+}
 
 /**
  * Calculate the length of a vector.
  * If you only need to compare lenghts, consider using the more efficient lengthSqr
  */
-// export function length(a: V3): number {
-//   return Math.sqrt(a[0] * a[0] + a[1] * a[1]);
-// }
+export function length(a: V3): number {
+  return Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+}
 
-// export function distance(a: V3, b: V3): number {
-//   return Math.sqrt(
-//     (a[0] - b[0]) * (a[0] - b[0]) + (a[0] - b[1]) * (a[0] - b[1])
-//   );
-// }
+export function distance(a: V3, b: V3): number {
+  return Math.sqrt(
+    (a[0] - b[0]) * (a[0] - b[0]) +
+      (a[1] - b[1]) * (a[1] - b[1]) +
+      (a[2] - b[2]) * (a[2] - b[2])
+  );
+}
